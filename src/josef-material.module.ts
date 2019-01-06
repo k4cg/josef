d84fa0de5de26a1c49c6d7c20058a3c25deab570
Poschi <development@poschi.net>
2019-01-06 18:59:21 +0100

    Basic material design

---
+++ src/josef-material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule, MatSidenavModule,  MatToolbarModule, MatListModule,   MatButtonModule } from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+  ],
+})
+export class JosefMaterialModule { }
